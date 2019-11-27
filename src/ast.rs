c330b657674362a03cf406102e61d444065f8dae
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2019-11-28 02:33:51 +0900

    ✨ Defined AST struct

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,25 @@
 use super::lexer;
+use std::boxed::Box;
 
-pub fn get() {
-    // todo
+#[derive(Debug)]
+pub struct AST {
+    token: i64,
+    node_left: Option<Box<AST>>,
+    node_right: Option<Box<AST>>
+}
+
+impl AST {
+    fn new(token: i64) -> Self {
+        Self {
+            token: token,
+            node_left: None,
+            node_right: None
+        }
+    }
+}
+
+pub fn get() -> AST {
+    let root = AST::new(0);
+    // println!("{:#?}", root);
+    root
 }
