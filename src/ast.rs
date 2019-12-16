4fb8706fa50cfd223e03803197a77a5720adf90a
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2019-12-16 16:44:16 +0900

    🚧 Updated AST

--- src/ast.rs
+++ src/ast.rs
@@ -3,28 +3,20 @@ use std::boxed::Box;
 #[derive(Debug)]
 pub struct ExprAST {
     token: i64,
-    llvm_ir: String,
-    node_left: Option<Box<ExprAST>>,
-    node_right: Option<Box<ExprAST>>
+    node: Vec<Option<Box<ExprAST>>>
 }
 
 impl ExprAST {
     fn new(token: i64) -> Self {
         Self {
             token: token,
-            llvm_ir: String::new(),
-            node_left: None,
-            node_right: None
+            node: Vec::with_capacity(32)
         }
     }
 
     fn _insert(&mut self,  token: i64) {
         let node = Self::new(token);
-        self.node_left = Some(Box::new(node));
-
-        self.llvm_ir = String::from(match token {
-            _ => "# None"
-        });
+        self.node.push(Some(Box::new(node)));
     }
 }
 
